e2a254ebf7df5d987c7a892b7c8a31e32fa9cc48
Joseph Van der Wee <jvanderwee@users.noreply.github.com>
2019-08-08 15:55:54 +0100

    Fixed formatting in TaxYear.kt

--- src/commonMain/kotlin/calculator/utils/TaxYear.kt
+++ src/commonMain/kotlin/calculator/utils/TaxYear.kt
@@ -16,18 +16,12 @@
 package calculator.utils
 
 import com.soywiz.klock.DateTime
-import com.soywiz.klock.DateTimeTz
 
 internal class TaxYear {
     fun currentTaxYear(): Int {
         val date = DateTime.nowLocal()
-        return if (date < firstDayOfTaxYear(date.yearInt))
-            date.yearInt - 1
-        else
-            date.yearInt
+        return if (date < firstDayOfTaxYear(date.yearInt)) date.yearInt - 1 else date.yearInt
     }
 
-    private fun firstDayOfTaxYear(year: Int): DateTimeTz {
-        return DateTime(year = year, month = 4, day = 6).local
-    }
+    private fun firstDayOfTaxYear(year: Int) = DateTime(year = year, month = 4, day = 6).local
 }
