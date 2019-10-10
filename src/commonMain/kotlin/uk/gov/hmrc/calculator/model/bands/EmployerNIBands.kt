33a5b8eeb5bb69c9cf5f6515096f050a0e18d409
Joseph Van der Wee <jvanderwee@users.noreply.github.com>
2019-10-10 11:42:48 +0100

    Create uk.gov.hmrc namespace

---
+++ src/commonMain/kotlin/uk/gov/hmrc/calculator/model/bands/EmployerNIBands.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.gov.hmrc.calculator.model.bands
+
+import uk.gov.hmrc.calculator.utils.UnsupportedTaxYear
+
+internal class EmployerNIBands(taxYear: Int) {
+
+    private val employerNIBands2019: List<EmployerNIBand> = listOf(
+        EmployerNIBand(0.0, 6136.00, 0.0),
+        EmployerNIBand(6136.0, 8632.00, 0.0),
+        EmployerNIBand(8632.0, 50000.00, 0.138),
+        EmployerNIBand(50000.0, -1.0, 0.138)
+    )
+
+    internal val bands: List<EmployerNIBand> = when (taxYear) {
+        2019 -> employerNIBands2019
+        else -> throw UnsupportedTaxYear("$taxYear")
+    }
+}
---
+++ src/commonMain/kotlin/uk/gov/hmrc/calculator/utils/String+Country.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.gov.hmrc.calculator.utils
+
+import uk.gov.hmrc.calculator.model.Country
+import uk.gov.hmrc.calculator.model.Country.ENGLAND
+import uk.gov.hmrc.calculator.model.Country.NONE
+import uk.gov.hmrc.calculator.model.Country.SCOTLAND
+import uk.gov.hmrc.calculator.model.Country.WALES
+
+internal fun String.toCountry(): Country {
+    return when (this) {
+        "NT" -> NONE
+        else -> when (this.first().toString()) {
+            "S" -> SCOTLAND
+            "C" -> WALES
+            else -> ENGLAND
+        }
+    }
+}
