dcb41546e1c4111728c18809806fff1e5b94dbe9
Christopher O'Brien <chris.obrien@digital.hmrc.gov.uk>
2019-10-21 11:22:28 +0100

    feature/HMA-1258-deploy-new-tax-calc - Updating script

--- package-ios.sh
+++ package-ios.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-export GITHUB_TOKEN="20a19f0825e0b0a9d0a5434da932a91988c8783d"
 if [ -z "$1" ]; then
     echo "ERROR: Please provide a git tag"
 else
     if echo "$1" | grep -Eq "^\d{0,3}\.\d{1}\.\d{1}$"; then
-      ./gradlew build
       DIRECTORY="Carthage"
       ZIP="TaxKalculator.framework.zip"
       if [ -d "$DIRECTORY" ]; then
@@ -13,11 +11,14 @@ else
       if [ -f "$ZIP" ]; then
         rm $ZIP
       fi
+      echo "INFO: Creating Carthage folder structure"
       mkdir $DIRECTORY && mkdir $DIRECTORY/Build && mkdir $DIRECTORY/Build/iOS
+      echo "INFO: Copying artifact"
       cp -r build/xcode-frameworks/TaxKalculator* $DIRECTORY/Build/iOS
+      echo "INFO: Zipping"
       zip -r TaxKalculator.framework.zip $DIRECTORY
-      echo "ghr run step"
-#      ghr $1 $ZIP
+      echo "INFO: Adding zipped artifact to release"
+      ghr "$1" $ZIP
     else
       echo "ERROR: Please use a valid git tag format e.g 1.0.3"
     fi
