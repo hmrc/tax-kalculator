891d28eb8c5e640a909282d61346806f455a7dc1
Joseph Van der Wee <jvanderwee@users.noreply.github.com>
2020-05-20 21:49:00 +0100

    HMA-3012 remove unused licenseString

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 import java.text.SimpleDateFormat
-import java.util.Calendar
 import java.util.Date
 import java.util.Properties
 import org.gradle.api.tasks.GradleBuild
@@ -52,22 +51,6 @@ repositories {
  ***********************************************************************************************************************/
 
 val frameworkName = "TaxKalculator"
-val licenseString = """/*
- * Copyright ${getYear()} HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-"""
 
 /***********************************************************************************************************************
  * Kotlin Configuration
@@ -250,9 +233,6 @@ tasks.jacocoTestCoverageVerification {
 /***********************************************************************************************************************
  * Custom Functions
  **********************************************************************************************************************/
-fun getYear(): String {
-    return Calendar.getInstance().get(Calendar.YEAR).toString()
-}
 
 fun getDate(): String {
     val date = Date()
