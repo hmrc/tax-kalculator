d03bcab08fd9041e511b7a06c2a5e0cb2b982a12
Chris Birch <3192947+chrisbirch@users.noreply.github.com>
2022-03-07 15:46:57 +0000

    [HMA-5336]

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TaxKalculator",
-            url: "https://github.com/IntenseBits/tax-calculator/raw/main/TaxKalculator.xcframework.zip",
+            url: "https://github.com/IntenseBits/tax-calculator/archive/refs/tags/0.0.1.zip",
             checksum: "110f5d0fb145a8347d0acec4bf0b272540d9d78a7952c2b29f7e477cbe34a754"
         ),
     ]
