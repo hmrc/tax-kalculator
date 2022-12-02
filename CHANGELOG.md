79b556e8f6942f245ecc66bc9d65fae94e0e3a22
chrisob55 <4126049+chrisob55@users.noreply.github.com>
2022-12-02 11:17:11 +0000

    Bump version to 2.7.1 and tagging release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+
+## [2.7.1] - 2022-12-02Z
 - Fixed issue with the `.jar` name for JVM consumers.
 
 ## [2.7.0] - 2022-11-24Z
--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TaxKalculator",
-            url: "https://github.com/hmrc/tax-kalculator/releases/download/2.7.0/TaxKalculator.xcframework.zip",
-            checksum: "6c9f09d7963d77a79d15a76b4473141e78da0894756ea312632ae7d873d8b8ad"
+            url: "https://github.com/hmrc/tax-kalculator/releases/download/2.7.1/TaxKalculator.xcframework.zip",
+            checksum: "80aa18c07f97db63d89e975c3a78c4741b2e26ca995465b4082331a6884c031c"
         ),
     ]
 )
