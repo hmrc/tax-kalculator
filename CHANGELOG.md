ca27333fa093de02ea1b88797fde2d54a5a5db54
ngoulongkam <25244131+ngoulongkam@users.noreply.github.com>
2023-03-13 11:22:31 +0000

    Bump version to 2.9.1 and tagging release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+
+## [2.9.1] - 2023-03-13Z
 - Downgrade Kotlin version for compatibility.
 
 ## [2.9.0] - 2023-03-09Z
--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TaxKalculator",
-            url: "https://github.com/hmrc/tax-kalculator/releases/download/2.9.0/TaxKalculator.xcframework.zip",
-            checksum: "d5a004f6a9c70e6d8941b54563cad55fd544a6f34f1233add00d8736cc222220"
+            url: "https://github.com/hmrc/tax-kalculator/releases/download/2.9.1/TaxKalculator.xcframework.zip",
+            checksum: "782207f2efecf9ccc2f651aa5fd47a221d2243d73e32fd10de55038bc025d996"
         ),
     ]
 )
