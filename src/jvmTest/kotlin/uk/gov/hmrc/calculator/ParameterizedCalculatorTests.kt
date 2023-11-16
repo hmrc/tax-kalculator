a9ba760b15e7c3ce215b6ddec08fb27c4c3d8c8d
ngoulongkam <25244131+ngoulongkam@users.noreply.github.com>
2023-11-16 17:55:58 +0000

    Added integration tests

--- src/jvmTest/kotlin/uk/gov/hmrc/calculator/ParameterizedCalculatorTests.kt
+++ src/jvmTest/kotlin/uk/gov/hmrc/calculator/ParameterizedCalculatorTests.kt
@@ -25,6 +25,7 @@ import org.junit.jupiter.params.provider.CsvFileSource
 import uk.gov.hmrc.calculator.model.Country
 import uk.gov.hmrc.calculator.model.PayPeriod
 import uk.gov.hmrc.calculator.model.TaxYear
+import uk.gov.hmrc.calculator.model.pension.AnnualPensionMethod
 import uk.gov.hmrc.calculator.utils.prettyPrintDataClass
 
 internal class ParameterizedCalculatorTests {
@@ -230,6 +231,120 @@ internal class ParameterizedCalculatorTests {
         assertEquals(PayPeriod.WEEKLY, response.weekly.payPeriod)
     }
 
+    @ParameterizedTest(name = "tax_code={0}, wages={1}, pay_period={2}, tax_year={3}, is_pension_age={4}")
+    @CsvFileSource(resources = ["/data2023_with_pension.csv"], numLinesToSkip = 1)
+    fun `Tax calculations 2023 with 10 percent Pension contribution`(
+        inputTaxCode: String,
+        inputWages: Double,
+        @ConvertWith(PayPeriodConverter::class) inputPayPeriod: PayPeriod,
+        @ConvertWith(TaxYearConverter::class) inputTaxYear: TaxYear,
+        inputIsPensionAge: Boolean,
+        @ConvertWith(AnnualPensionMethodConverter::class) inputPensionMethod: AnnualPensionMethod,
+        inputPensionAmount: Double?,
+        inputPensionPercentage: Double?,
+        @ConvertWith(CountryConverter::class) expectedCountry: Country,
+        expectedYearlyNiEmployee: Double,
+        expectedYearlyNiEmployer: Double,
+        expectedYearlyIncomeTax: Double,
+        expectedYearlyTotalDeduction: Double,
+        expectedYearlyTakeHome: Double,
+        expectedYearlyWages: Double,
+        expectedYearlyTaxFreeAmount: Double,
+        expectedYearlyKCodeAdjustment: Double?,
+        expectedIsKCode: Boolean
+    ) {
+        val response = Calculator(
+            taxCode = inputTaxCode,
+            wages = inputWages,
+            payPeriod = inputPayPeriod,
+            taxYear = inputTaxYear,
+            pensionMethod = inputPensionMethod,
+            pensionYearlyAmount = inputPensionAmount,
+            pensionPercentage = inputPensionPercentage,
+            isPensionAge = inputIsPensionAge
+        ).run()
+
+        assertEquals(expectedCountry, response.country, "Country did not match")
+        assertEquals(expectedIsKCode, response.isKCode)
+
+        val yearlyPeriod = response.yearly
+        println(inputTaxCode)
+        println(yearlyPeriod.prettyPrintDataClass())
+        println("expectedYearlyNiEmployer=$expectedYearlyNiEmployer, yearlyPeriod.employersNI=$yearlyPeriod.employersNI")
+        assertEquals(PayPeriod.YEARLY, yearlyPeriod.payPeriod)
+        assertEquals(expectedYearlyNiEmployee, yearlyPeriod.employeesNI, "Yearly employee NI did not match")
+        assertEquals(expectedYearlyNiEmployer, yearlyPeriod.employersNI, "Yearly employer NI did not match")
+        assertEquals(expectedYearlyIncomeTax, yearlyPeriod.taxToPay, "Yearly income tax did not match")
+        assertEquals(expectedYearlyTotalDeduction, yearlyPeriod.totalDeductions, "Yearly total deductions did not match")
+        assertEquals(expectedYearlyTakeHome, yearlyPeriod.takeHome, "Yearly take home did not match")
+        assertEquals(expectedYearlyWages, yearlyPeriod.wages, "Yearly wages did not match")
+        assertEquals(expectedYearlyTaxFreeAmount, yearlyPeriod.taxFree, "Yearly tax free amount did not match")
+        assertEquals(expectedYearlyKCodeAdjustment, yearlyPeriod.kCodeAdjustment, "Yearly K code adjustment did not match")
+        assertFalse(yearlyPeriod.maxTaxAmountExceeded)
+
+        assertEquals(PayPeriod.MONTHLY, response.monthly.payPeriod)
+        assertEquals(PayPeriod.FOUR_WEEKLY, response.fourWeekly.payPeriod)
+        assertEquals(PayPeriod.WEEKLY, response.weekly.payPeriod)
+    }
+
+    @ParameterizedTest(name = "tax_code={0}, wages={1}, pay_period={2}, tax_year={3}, is_pension_age={4}")
+    @CsvFileSource(resources = ["/data2023_with_student_loan.csv"], numLinesToSkip = 1)
+    fun `Tax calculations 2023 with student loan`(
+        inputTaxCode: String,
+        inputWages: Double,
+        @ConvertWith(PayPeriodConverter::class) inputPayPeriod: PayPeriod,
+        @ConvertWith(TaxYearConverter::class) inputTaxYear: TaxYear,
+        inputIsPensionAge: Boolean,
+        inputHasPlanOne: Boolean,
+        inputHasPlanTwo: Boolean,
+        inputHasPlanFour: Boolean,
+        inputHasPostgraduatePlan: Boolean,
+        @ConvertWith(CountryConverter::class) expectedCountry: Country,
+        expectedYearlyNiEmployee: Double,
+        expectedYearlyNiEmployer: Double,
+        expectedYearlyIncomeTax: Double,
+        expectedYearlyTotalDeduction: Double,
+        expectedYearlyTakeHome: Double,
+        expectedYearlyWages: Double,
+        expectedYearlyTaxFreeAmount: Double,
+        expectedYearlyKCodeAdjustment: Double?,
+        expectedIsKCode: Boolean
+    ) {
+        val response = Calculator(
+            taxCode = inputTaxCode,
+            wages = inputWages,
+            payPeriod = inputPayPeriod,
+            taxYear = inputTaxYear,
+            isPensionAge = inputIsPensionAge,
+            hasStudentLoanPlanOne = inputHasPlanOne,
+            hasStudentLoanPlanTwo = inputHasPlanTwo,
+            hasStudentLoanPlanFour = inputHasPlanFour,
+            hasStudentLoanPostgraduatePlan = inputHasPostgraduatePlan,
+        ).run()
+
+        assertEquals(expectedCountry, response.country, "Country did not match")
+        assertEquals(expectedIsKCode, response.isKCode)
+
+        val yearlyPeriod = response.yearly
+        println(inputTaxCode)
+        println(yearlyPeriod.prettyPrintDataClass())
+        println("expectedYearlyNiEmployer=$expectedYearlyNiEmployer, yearlyPeriod.employersNI=$yearlyPeriod.employersNI")
+        assertEquals(PayPeriod.YEARLY, yearlyPeriod.payPeriod)
+        assertEquals(expectedYearlyNiEmployee, yearlyPeriod.employeesNI, "Yearly employee NI did not match")
+        assertEquals(expectedYearlyNiEmployer, yearlyPeriod.employersNI, "Yearly employer NI did not match")
+        assertEquals(expectedYearlyIncomeTax, yearlyPeriod.taxToPay, "Yearly income tax did not match")
+        assertEquals(expectedYearlyTotalDeduction, yearlyPeriod.totalDeductions, "Yearly total deductions did not match")
+        assertEquals(expectedYearlyTakeHome, yearlyPeriod.takeHome, "Yearly take home did not match")
+        assertEquals(expectedYearlyWages, yearlyPeriod.wages, "Yearly wages did not match")
+        assertEquals(expectedYearlyTaxFreeAmount, yearlyPeriod.taxFree, "Yearly tax free amount did not match")
+        assertEquals(expectedYearlyKCodeAdjustment, yearlyPeriod.kCodeAdjustment, "Yearly K code adjustment did not match")
+        assertFalse(yearlyPeriod.maxTaxAmountExceeded)
+
+        assertEquals(PayPeriod.MONTHLY, response.monthly.payPeriod)
+        assertEquals(PayPeriod.FOUR_WEEKLY, response.fourWeekly.payPeriod)
+        assertEquals(PayPeriod.WEEKLY, response.weekly.payPeriod)
+    }
+
     class PayPeriodConverter : ArgumentConverter {
         override fun convert(source: Any, context: ParameterContext?) = PayPeriod.valueOf(source as String)
     }
@@ -240,5 +355,9 @@ internal class ParameterizedCalculatorTests {
 
     class TaxYearConverter : ArgumentConverter {
         override fun convert(source: Any, context: ParameterContext?) = TaxYear.valueOf(source as String)
+    }
+
+    class AnnualPensionMethodConverter : ArgumentConverter {
+        override fun convert(source: Any?, context: ParameterContext?) = AnnualPensionMethod.valueOf(source as String)
     }
 }
