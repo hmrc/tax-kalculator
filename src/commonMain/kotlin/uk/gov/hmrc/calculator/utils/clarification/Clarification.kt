ee59c04fd370f427734fd51d74df7b3be5bdde45
ngoulongkam <25244131+ngoulongkam@users.noreply.github.com>
2024-06-13 15:00:15 +0100

    detekt

--- src/commonMain/kotlin/uk/gov/hmrc/calculator/utils/clarification/Clarification.kt
+++ src/commonMain/kotlin/uk/gov/hmrc/calculator/utils/clarification/Clarification.kt
@@ -23,11 +23,17 @@ enum class Clarification(val priority: Int, private val description: String) {
     NO_TAX_CODE_SUPPLIED(4, "Did not provide a tax code."),
     SCOTTISH_INCOME_APPLIED(5, "Applied Scottish Income Tax rates."),
     SCOTTISH_CODE_BUT_OTHER_RATE(6, "Applied Scottish Income Tax rates because you used a Scottish tax code."),
-    NON_SCOTTISH_CODE_BUT_SCOTTISH_RATE(7, "Applied Scottish Income Tax rates as you said you pay Scottish Income Tax."),
+    NON_SCOTTISH_CODE_BUT_SCOTTISH_RATE(
+        7,
+        "Applied Scottish Income Tax rates as you said you pay Scottish Income Tax."
+    ),
     K_CODE(8, "Tax code started with K."),
     PENSION_EXCEED_ANNUAL_ALLOWANCE(9, "Pension contribution amount is higher than annual allowance."),
     PENSION_BELOW_ANNUAL_ALLOWANCE(10, "Pension contribution amount is below than annual allowance."),
-    INCOME_BELOW_STUDENT_AND_POSTGRAD_LOAN(11, "Income is below the level needed to start repaying student/postgrad loan."),
+    INCOME_BELOW_STUDENT_AND_POSTGRAD_LOAN(
+        11,
+        "Income is below the level needed to start repaying student/postgrad loan."
+    ),
     INCOME_BELOW_STUDENT_LOAN(12, "Income is below the level needed to start repaying student loan."),
     INCOME_BELOW_POSTGRAD_LOAN(13, "Income is below the level needed to start repaying postgrad loan."),
     INCOME_BELOW_STUDENT_BUT_ABOVE_POSTGRAD_LOAN(
