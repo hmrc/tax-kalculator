54d98be9c6e8d672ae8965ab08c99c1fdb2fec00
rajesh-1394 <177030043+rajesh-1394@users.noreply.github.com>
2026-02-13 12:57:16 +0000

    HMA-11199 Updated test cases with new bands, thresholds, tax rates, student loan thresholds and the test csv files as per new 2026-27 tax plan

--- src/commonTest/kotlin/uk/gov/hmrc/calculator/CalculatorUtilsTests.kt
+++ src/commonTest/kotlin/uk/gov/hmrc/calculator/CalculatorUtilsTests.kt
@@ -70,7 +70,7 @@ internal class CalculatorUtilsTests {
         val result = CalculatorUtils.defaultTaxCode()
 
         assertEquals(
-            2025,
+            2026,
             result.year
         )
         assertEquals(
